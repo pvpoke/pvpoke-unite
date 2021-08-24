295e1383955faa0e6a9db48e5ebb957d6367ae4a
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-08-24 17:30:45 -0500

    Initial analytics setup

---
+++ src/modules/analytics.php
@@ -0,0 +1,11 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $UA; ?>"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '<?php echo $UA; ?>');
+
+  var UA_ID = '<?php echo $UA; ?>';
+</script>
