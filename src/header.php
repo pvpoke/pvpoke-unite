482a6801642367fa287b702bb1aa8d09f1fc1b75
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-09-10 19:10:39 -0500

    Adjusted move image asset size
    Adjusted to match styling in Select Window.

--- src/header.php
+++ src/header.php
@@ -58,7 +58,7 @@ if(! isset($OG_IMAGE)){
 <link rel="manifest" href="<?php echo $WEB_ROOT; ?>data/manifest.json?v=1">
 
 <link rel="icon" href="<?php echo $WEB_ROOT; ?>img/favicon.png">
-<link rel="stylesheet" type="text/css" href="<?php echo $WEB_ROOT; ?>css/style.css?v=8">
+<link rel="stylesheet" type="text/css" href="<?php echo $WEB_ROOT; ?>css/style.css?v=9">
 
 <script src="<?php echo $WEB_ROOT; ?>js/libs/jquery-3.3.1.min.js"></script>
 
